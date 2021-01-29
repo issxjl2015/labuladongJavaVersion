fdb37a889a7e0f0ec0956d66d7d2ad901ec00e33
issxjl2015 <issxjl2015@outlook.com>
2021-01-29 23:26:43 +0800

    利用后序遍历计算Lc 124 最大路径和

---
+++ labuladongAlgo/src/main/java/org/issxjl2015/chapter1/algo_1_算法刷题框架思维/algo_datastrucBaseOp/Lc_124_二叉树中的最大路径和.java
@@ -0,0 +1,34 @@
+package org.issxjl2015.chapter1.algo_1_算法刷题框架思维.algo_datastrucBaseOp;
+
+import org.issxjl2015.common.TreeNode;
+
+/**
+ * 124. 二叉树中的最大路径和
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。该路径 至少包含一个 节点，且不一定经过根节点。
+ *
+ * 路径和 是路径中各节点值的总和。
+ *
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ */
+public class Lc_124_二叉树中的最大路径和 {
+
+    int ans = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        onePathSum(root);
+        return ans;
+    }
+
+    public int onePathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = Math.max(0, onePathSum(root.left));
+        int right = Math.max(0, onePathSum(root.right));
+
+        // 后续遍历
+        ans = Math.max(ans, left + right + root.val);
+        return Math.max(left, right) + root.val;
+    }
+
+}
---
+++ labuladongAlgo/src/test/java/org/issxjl2015/chapter1/Lc_124_Test.java
@@ -0,0 +1,24 @@
+package org.issxjl2015.chapter1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.issxjl2015.chapter1.algo_1_算法刷题框架思维.algo_datastrucBaseOp.Lc_124_二叉树中的最大路径和;
+import org.issxjl2015.common.TreeNode;
+import org.junit.Test;
+
+@Slf4j
+public class Lc_124_Test {
+    @Test
+    public void test1() {
+        Lc_124_二叉树中的最大路径和 solution = new Lc_124_二叉树中的最大路径和();
+        TreeNode root = new TreeNode();
+        root.val = 1;
+        root.left = new TreeNode();
+        root.left.val = 2;
+        root.right = new TreeNode();
+        root.right.val = 3;
+
+        int ans = solution.maxPathSum(root);
+        System.out.println(ans);
+    }
+
+}
