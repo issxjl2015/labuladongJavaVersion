163bb662a4e34b0ed8b30d419d066138a3916066
issxjl2015 <issxjl2015@outlook.com>
2021-03-05 21:45:29 +0800

    找到字符串中所有字符异位词，用滑动窗口解决

---
+++ labuladongAlgo/src/main/java/org/issxjl2015/chapter1/algo_7_滑动窗口算法解题套路框架/lc_438_找到字符串中所有字母异位词.java
@@ -0,0 +1,53 @@
+package org.issxjl2015.chapter1.algo_7_滑动窗口算法解题套路框架;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class lc_438_找到字符串中所有字母异位词 {
+
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (int i = 0; i < p.length(); i++) {
+            need.put(p.charAt(i), need.getOrDefault(p.charAt(i), 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int vaild = 0;
+        while (right < s.length()) {
+            // 要从右侧移入窗口的字符
+            char c = s.charAt(right);
+            right++;
+
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (need.get(c).equals(window.get(c))) {
+                    vaild++;
+                }
+            }
+
+            while (right - left >= p.length()) {
+                // 判断是否找到合适的子串
+                if (vaild == need.size()) {
+                    // 将索引加到ans结果中
+                    ans.add(left);
+                }
+                // 要从左侧移出窗口的字符
+                char d = s.charAt(left);
+                left++;
+                // 更新窗口内的一系列的数据
+                if (need.containsKey(d)) {
+                    if (need.get(d).equals(window.get(d))) {
+                        vaild--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ labuladongAlgo/src/test/java/org/issxjl2015/chapter1/lc_438_找到字符串中所有字母异位词_test.java
@@ -0,0 +1,52 @@
+package org.issxjl2015.chapter1;
+
+import org.issxjl2015.chapter1.algo_7_滑动窗口算法解题套路框架.lc_438_找到字符串中所有字母异位词;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class lc_438_找到字符串中所有字母异位词_test {
+
+    @Test
+    public void test1() {
+        lc_438_找到字符串中所有字母异位词 solution = new lc_438_找到字符串中所有字母异位词();
+        String s = "cbaebabacd";
+        String p = "abc";
+        List<Integer> ans = solution.findAnagrams(s, p);
+        int[] ansInt = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            ansInt[i] = ans.get(i);
+        }
+        int[] expectedsInt = {0, 6};
+        Assert.assertArrayEquals(expectedsInt, ansInt);
+    }
+
+    @Test
+    public void test2() {
+        lc_438_找到字符串中所有字母异位词 solution = new lc_438_找到字符串中所有字母异位词();
+        String s = "abab";
+        String p = "ab";
+        List<Integer> ans = solution.findAnagrams(s, p);
+        int[] ansInt = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            ansInt[i] = ans.get(i);
+        }
+        int[] expectedsInt = {0, 1, 2};
+        Assert.assertArrayEquals(expectedsInt, ansInt);
+    }
+
+    @Test
+    public void test3() {
+        lc_438_找到字符串中所有字母异位词 solution = new lc_438_找到字符串中所有字母异位词();
+        String s = "baa";
+        String p = "aa";
+        List<Integer> ans = solution.findAnagrams(s, p);
+        int[] ansInt = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            ansInt[i] = ans.get(i);
+        }
+        int[] expectedsInt = {1};
+        Assert.assertArrayEquals(expectedsInt, ansInt);
+    }
+}
