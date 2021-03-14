a7e438e221a311e14dd8d1762339e76bfdaa87ba
issxjl2015 <issxjl2015@outlook.com>
2021-03-14 11:30:15 +0800

    求素数的代码优化过程

---
+++ labuladongAlgo/src/main/java/org/issxjl2015/chapter5/algo_1_如何高效寻找素数/algo_素数个数.java
@@ -0,0 +1,23 @@
+package org.issxjl2015.chapter5.algo_1_如何高效寻找素数;
+
+public class algo_素数个数 {
+
+    public int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrimes(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isPrimes(int n) {
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ labuladongAlgo/src/main/java/org/issxjl2015/chapter5/algo_1_如何高效寻找素数/algo_素数个数_筛数法_优化.java
@@ -0,0 +1,27 @@
+package org.issxjl2015.chapter5.algo_1_如何高效寻找素数;
+
+import java.util.Arrays;
+
+public class algo_素数个数_筛数法_优化 {
+
+    public int countPrimes(int n) {
+        boolean[] isPrimes = new boolean[n];
+        Arrays.fill(isPrimes, true);
+        for (int i = 2; i * i < n; i++) {
+            if (isPrimes[i]) {
+                for (int j = i * i; j <= n; j +=i) {
+                    isPrimes[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i =  2; i < n; i++) {
+            if (isPrimes[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+}
